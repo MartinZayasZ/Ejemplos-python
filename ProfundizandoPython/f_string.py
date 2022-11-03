4e7837aa89674aaeb9cbef3d9a870023543e4c80
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-03 09:43:28 -0700

    f string

---
+++ ProfundizandoPython/f_string.py
@@ -0,0 +1,10 @@
+
+nombre = 'Juan'
+edad = 28
+sueldo = 3000
+mensaje_con_formato = f'Nombre {nombre}, Edad {edad}, Sueldo {sueldo:.2f} '
+
+print(mensaje_con_formato)
+
+# Método print
+print(nombre, edad, sueldo, sep=', ')
