d21a319902beb06ca956fa9206312f014f956b10
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-03 10:28:07 -0700

    multiplicación str

---
+++ ProfundizandoPython/multiplicacion_str.py
@@ -0,0 +1,13 @@
+
+
+resultado = 5 * 'Hola'
+print(resultado)
+
+# Multiplicación de tublas
+resultado = 5*('Hola', 10)
+print(f'Resultado: {resultado}')
+
+# Multiplicación listas
+resultado = 10*[{}]
+print(f'Resultado: {resultado}')
+print(f'Cantidad: {len(resultado)}')
