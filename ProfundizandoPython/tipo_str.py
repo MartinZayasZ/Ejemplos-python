4a053b77a99a5e6e5e034120b4f9b4cf2ba02de8
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-02 22:16:58 -0700

    str join

--- ProfundizandoPython/tipo_str.py
+++ ProfundizandoPython/tipo_str.py
@@ -10,6 +10,26 @@ from mi_clase import MiClase
 
 
 # help(MiClase)
-print(MiClase.__doc__)
-print(MiClase.__init__.__doc__)
-print(MiClase.mi_metodo.__doc__)
+# print(MiClase.__doc__)
+# print(MiClase.__init__.__doc__)
+# print(MiClase.mi_metodo.__doc__)
+
+
+# help(str.join)
+tupla_str = ('hola', 'mundo', 'universidad', 'python')
+mensaje = ' '.join(tupla_str)
+print(f'mensaje: {mensaje}')
+
+lista_cursos = ['java', 'python', 'angular']
+mensaje = ', '.join(lista_cursos)
+print(f'mensaje: {mensaje}')
+
+cadena = 'holamundo'
+mensaje = '.'.join(cadena)
+print(f'mensaje: {mensaje}')
+
+diccionario = {'nombre':'Juan', 'apellido':'Perez', 'edad':'18'}
+llaves = '-'.join(diccionario.keys())
+valores = '-'.join(diccionario.values())
+print(f'llaves: {llaves}')
+print(f'valores: {valores}')
