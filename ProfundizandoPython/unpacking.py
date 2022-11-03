1c8e6f1021403882a22391d4b7e7e37eb4e3289e
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-03 13:55:52 -0700

    unpacking

---
+++ ProfundizandoPython/unpacking.py
@@ -0,0 +1,32 @@
+valores = 1,2,3
+print(valores)
+print(type(valores))
+
+valor1, valor2, valor3 = 1, 2, 3
+print(valor1, valor2, valor3)
+
+valor1, _, valor3 = 1, 2, 3
+print(valor1, valor3)
+
+valor1, valor2, *valor3 = 1, 2, 3, 4, 5, 6, 7, 8, 9
+print(valor1, valor2, valor3)
+
+valor1, valor2, *valor3, valor4, valor5 = 1, 2, 3, 4, 5, 6, 7, 8, 9
+print(valor1, valor2, valor3, valor4, valor5)
+
+
+valor1, valor2, *valor3, valor4, valor5 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(valor1, valor2, valor3, valor4, valor5)
+
+
+def regresa_varios_datos():
+    return 1, 2, 3
+
+valor1, valor2, valor3 = regresa_varios_datos()
+print(valor1, valor2, valor3)
+
+
+# help(str.partition)
+hora, separador, minutos = '17:20'.partition(':')
+# print(type(variable))
+print(hora, separador, minutos)
