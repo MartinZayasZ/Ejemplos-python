ac36fb8cc19de8b677eebd23ed12e2cb626f9020
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-02 21:53:32 -0700

    str y docstring

---
+++ ProfundizandoPython/tipo_str.py
@@ -0,0 +1,15 @@
+from mi_clase import MiClase
+# Profundizando en el tipo str
+
+# Concatenación automática en python
+# variable = 'Adios'
+# mensaje = 'Hola' 'Mundo' + variable
+# mensaje += 'Universidad' 'Python'
+# print(mensaje.upper())
+
+
+
+# help(MiClase)
+print(MiClase.__doc__)
+print(MiClase.__init__.__doc__)
+print(MiClase.mi_metodo.__doc__)
