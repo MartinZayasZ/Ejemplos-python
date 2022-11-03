7030a56492ba6f696c76f71ae1ce49e8e2f9de6f
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-03 10:57:20 -0700

    caracteres de escape

---
+++ ProfundizandoPython/caracteres_de_escape.py
@@ -0,0 +1,13 @@
+
+resultado = 'Hola \' Mundo'
+print(f'Resultado: {resultado}')
+
+resultado = 'Se va a eliminar el punto.\b'
+print(f'Resultado: {resultado}')
+
+resultado = 'c:\\directorio\\juan'
+print(f'Resultado: {resultado}')
+
+# raw string
+resultado = r'Cadena con {resultado} \n salto de línea'
+print(f'Resultado: {resultado}')
