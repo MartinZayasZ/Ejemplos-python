0c79a7f1040648a230f48224fdacf21a96dfb5ee
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-13 03:25:25 -0700

    herencia multiple mro

--- ProfundizandoPython/mro_ejemplo.py
+++ ProfundizandoPython/mro_ejemplo.py
@@ -7,19 +7,24 @@ class Clase1:
 class Clase2(Clase1):
     def __init__(self):
         print('Clase2.__init__')
+        super().__init__()
     def metodo(self):
         print('Método Clase2')
+        super().metodo()
 
 class Clase3(Clase1):
     def __init__(self):
         print('Clase3.__init__')
+        super().__init__()
     def metodo(self):
         print('Método Clase3')
+        super().metodo()
 
 class Clase4(Clase2, Clase3):
     pass
     def metodo(self):
         print('Método Clase4')
+        super().metodo()
 
 
 # Creamos un objeto de la clase 4
