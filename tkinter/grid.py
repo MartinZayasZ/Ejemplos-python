c4bfc4c5fec6a8488f76c2a122cceb49f86ed3d3
MartinZayasZ <zayasmartin70@gmail.com>
2022-11-13 17:04:40 -0700

    tkinter grid 2

--- tkinter/grid.py
+++ tkinter/grid.py
@@ -6,17 +6,33 @@ ventana.geometry('600x400')
 ventana.title('Manejo grid')
 ventana.iconbitmap('icono.ico')
 
+#configurar el grid
+ventana.rowconfigure(0, weight=2)
+ventana.rowconfigure(1, weight=10)
+ventana.columnconfigure(0, weight=1)
+ventana.columnconfigure(1, weight=5)
+
 def evento1():
     boton1.config(text='Botón 1 presionado')
 def evento2():
     boton2.config(text='Botón 2 presionado')
+def evento4():
+    boton4.config(text='Botón 4 presionado', fg='blue', relief=tk.GROOVE, bg='yellow')
 
 #definimos dos botones
 boton1 = ttk.Button(ventana, text='Botón 1', command=evento1)
-boton1.grid(row=0, column=0, sticky='W')
+boton1.grid(row=0, column=0, sticky='NSWE')
 
 # N(arriba), E(derecha), S(abajo), W(izquierda),
 boton2 = ttk.Button(ventana, text='Botón 2', command=evento2)
-boton2.grid(row=1, column=0, sticky='WE')
+boton2.grid(row=1, column=0, sticky='NSWE')
+
+# BOTON3
+boton3 = ttk.Button(ventana, text='Botón 3')
+boton3.grid(row=0, column=1, sticky='NSWE')
+
+#boton 4
+boton4 = tk.Button(ventana, text='Botón 4', command=evento4)
+boton4.grid(row=1, column=1, sticky='NSWE')
 
 ventana.mainloop()
